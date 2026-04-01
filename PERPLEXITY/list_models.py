15882845ed86fe44bf4535547f6a799a908a1feb
manikikkara18 <manikikkara57@gmail.com>
2026-04-01 14:27:24 +0530

    Delete PERPLEXITY directory

--- PERPLEXITY/list_models.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-API_KEY = "AIzaSyDVCsVZGJFzoX8YbfOjwCGwET-IpD7V4Z4"
-url = f"https://generativelanguage.googleapis.com/v1/models?key={API_KEY}"
-
-response = requests.get(url)
-print(response.json())
